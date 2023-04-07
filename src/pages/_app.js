a486ac5606ac40f8339a27a24dd4cac5de53867e
wibawasuyadnya <dekgusnfs@gmail.com>
2023-04-08 03:03:59 +0800

    final fix

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,7 +2,9 @@ import '../styles/globals.css'
 import React, {useEffect} from 'react';
 import { ThemeProvider } from '../context/theme'
 import { Inter } from '@next/font/google'
-import { initGA, logPageView } from '../../analytics';
+import { initGA, logPageView } from '../../analytics'
+import { Analytics } from '@vercel/analytics/react'
+
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -17,6 +19,7 @@ function App({ Component, pageProps }) {
     <main className={inter.className}>
       <ThemeProvider>
           <Component {...pageProps} />
+          <Analytics />
       </ThemeProvider>
     </main>
   )
