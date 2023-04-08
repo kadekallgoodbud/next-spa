033277c6f746a277e888a83f2acd05ce302f8ac7
wibawasuyadnya <dekgusnfs@gmail.com>
2023-04-08 13:56:16 +0800

    upgrade pwa version

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
-/** @type {import('next').NextConfig} */
-const NextConfig = {
-  reactStrictMode: false,
-  compiler: {
-    styledComponents: {
-      ssr: true,
-    }
-  },
-}
-module.exports = NextConfig
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  //disable: process.env.NODE_ENV === 'development',
+  // register: true,
+  // scope: '/app',
+  sw: 'service-worker.js',
+  //...
+})
+
+module.exports = withPWA({
+  // next.js config
+})
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -41,7 +41,7 @@ export default class MyDocument extends Document {
           <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
           <link rel="icon" href="/logoAW.webp" sizes="32x32" />
           <link rel="icon" href="/logoAW.webp" sizes="16x16" />
-          <link rel="manifest" href="/site.webmanifest" />
+          <link rel="manifest" href="/manifest.json" />
           <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
           <meta name="msapplication-TileImage" content="/mstile-144x144.png" />
           <meta name="msapplication-config" content="/browserconfig.xml" />
