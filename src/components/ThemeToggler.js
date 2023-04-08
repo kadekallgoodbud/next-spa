15029933720bd9fb13528a829f048d5f3a02a71f
wibawasuyadnya <dekgusnfs@gmail.com>
2023-04-08 21:02:30 +0800

    fix useeffect on togglerdarkmode

--- src/components/ThemeToggler.js
+++ src/components/ThemeToggler.js
@@ -1,17 +1,10 @@
-import { useContext, useState, useEffect } from 'react';
+import { useContext, useState } from 'react';
 import { ThemeContext } from '../context/theme';
 import { DarkModeSwitch } from 'react-toggle-dark-mode';
 
 export default function ButtonToggler() {
     const [{ themeName, toggleTheme }] = useContext(ThemeContext);
     const [isDarkMode,setDarkMode] = useState(false);
-
-    useEffect(() => {
-        // Set the body background color and transition duration based on the theme
-        const body = document.body;
-        body.style.backgroundColor = themeName === 'dark' ? !isDarkMode : isDarkMode;
-        body.style.transitionDuration = '0.5s';
-      }, [themeName]);
 
     //set DarkMode if it's checked 
 
