3d44fac665e72391ca82ebdc0c606bb15324b458
kadekallgoodbud <120998889+kadekallgoodbud@users.noreply.github.com>
2023-05-02 14:37:34 +0800

    Update HTTP3 Polyfill to Nextjs
    Update http3 functionality with polyfill

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
-/** @type {import('next').NextConfig} */
+// Import the necessary packages
+const withPWA = require('next-pwa');
+const { withPolyfills } = require('next-polyfill');
+
+// Define your Next.js configuration
 const NextConfig = {
   reactStrictMode: false,
   api: {
@@ -9,17 +13,31 @@ const NextConfig = {
       ssr: true,
     },
   },
-}
+};
 
-const withPWA = require('next-pwa')({
-  dest: 'public',
-  register: true,
-  skipWaiting: true,
-  // scope: '/app',
-  sw: 'service-worker.js',
-  //...
-})
+// Add polyfills for necessary browser APIs
+const polyfills = ['Promise', 'fetch'];
 
-module.exports = withPWA({
-  ...NextConfig
-})
+// Define HTTP/3 headers
+const http3Headers = async () => {
+  return [
+    {
+      source: '/',
+      headers: [
+        {
+          key: 'Alt-Svc',
+          value: 'h3-29=":443"; ma=3600',
+        },
+      ],
+    },
+  ];
+};
+
+// Export your Next.js configuration with PWA and polyfill support
+module.exports = withPWA(
+  withPolyfills({
+    polyfills,
+    http3Headers,
+    ...NextConfig,
+  })
+);
